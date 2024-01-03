7a711363ab4b94099ca6b8e7321556f9946c12f3
Tanguri <jungwontwo2@naver.com>
2024-01-03 16:27:44 +0900

    User Entity 생성

--- src/main/java/Tanguri/BasicBoard/domain/entity/User.java
+++ src/main/java/Tanguri/BasicBoard/domain/entity/User.java
@@ -1,4 +1,27 @@
 package Tanguri.BasicBoard.domain.entity;
 
+import Tanguri.BasicBoard.domain.UserRole;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Getter
 public class User {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;            // DB에서 구분할 아이디
+
+    private String loginId;     //로그인할 때 사용하는 아이디
+    private String password;    //비밀번호
+    private String nickname;    //닉네임
+
+    private UserRole userRole;  //권한
 }
