5ea9d6e478609aae44474e1ccf5724b11ce75f52
Tanguri <jungwontwo2@naver.com>
2024-01-24 13:15:34 +0900

    UserRepository를 JPARepository로 변경

--- src/main/java/Tanguri/BasicBoard/repository/UserRepository.java
+++ src/main/java/Tanguri/BasicBoard/repository/UserRepository.java
@@ -1,52 +1,11 @@
 package Tanguri.BasicBoard.repository;
 
 import Tanguri.BasicBoard.domain.entity.User;
-import jakarta.persistence.EntityManager;
-import jakarta.persistence.NoResultException;
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Transactional;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Optional;
 
-@Repository
-@RequiredArgsConstructor
-@Transactional(readOnly = true)
-public class UserRepository {
-
-    private final EntityManager em;
-
-    @Transactional
-    public void save(User user){
-        if(user.getId()==null){
-            em.persist(user);
-        }
-        else {
-            em.merge(user);
-        }
-    }
-
-    public User findOne(Long id){
-        return em.find(User.class, id);
-    }
-
-    public User findByNickname(String nickname){
-        try{
-            return em.createQuery("select m from User m where m.nickname = :nickname",User.class)
-                    .setParameter("nickname",nickname)
-                    .getSingleResult();
-        }catch (NoResultException e){
-            return null;
-        }
-    }
-
-    public Optional<User> findByLoginId(String loginId){
-        return findAll().stream().filter(m->m.getLoginId().equals(loginId)).findAny();
-    }
-
-    public List<User> findAll(){
-        return em.createQuery("select m from User m",User.class).getResultList();
-    }
+public interface UserRepository extends JpaRepository<User,Long> {
+    User findByNickname(String nickname);
+    Optional<User> findByLoginId(String loginId);
 }
